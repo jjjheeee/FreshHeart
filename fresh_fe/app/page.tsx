3485a69405afb6c1a1de3b9afb1cb78e9f658594
wogml8524 <wogml8524@naver.com>
2024-11-25 16:54:04 +0900

    메인 페이지 템플릿

--- fresh_fe/app/page.tsx
+++ fresh_fe/app/page.tsx
@@ -1,7 +1,28 @@
-import Image from "next/image";
+"use client"
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import AppAppBar from './components/AppAppBar';
+import MainContent from './components/MainContent';
+import Latest from './components/Latest';
+import Footer from './components/Footer';
+import AppTheme from './shared-theme/AppTheme';
 
-export default function Home() {
+export default function Blog(props: { disableCustomTheme?: boolean }) {
   return (
-    <div>next.js start</div>
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+
+      <AppAppBar />
+      <Container
+        maxWidth="lg"
+        component="main"
+        sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
+      >
+        <Latest />
+        <MainContent />
+        <Footer />
+      </Container>
+    </AppTheme>
   );
 }
